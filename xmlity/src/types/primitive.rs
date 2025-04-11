ed2235e924171a3f00a4bfa0df14b29472c369cd
Lukas Friman <lukas@dreamplay.net>
2025-04-11 16:25:41 +0200

    feat(core): Adds more implementations for Rust primitives. (#13)
    * Adds DeserializationGroup/SerializationGroup to Option<T>.
    
    * Deserialize impl for bool.
    
    * Added usize, isize, f32 and f64 to FromStr/ToString primitive implementations.

--- xmlity/src/types/primitive.rs
+++ xmlity/src/types/primitive.rs
@@ -1,4 +1,7 @@
-use crate::de::{Deserialize, Deserializer};
+use core::fmt;
+use std::ops::Deref;
+
+use crate::de::{self, Deserialize, Deserializer};
 use crate::ser::{Serialize, Serializer};
 use crate::types::string::FromStrVisitor;
 
@@ -14,7 +17,9 @@ macro_rules! impl_serialize_for_primitive {
   };
 }
 
-impl_serialize_for_primitive!(u8, u16, u32, u64, u128, i8, i16, i32, i64, i128);
+impl_serialize_for_primitive!(
+    u8, u16, u32, u64, u128, i8, i16, i32, i64, i128, usize, isize, f32, f64
+);
 
 macro_rules! impl_deserialize_for_primitive {
   ($($t:ty),*) => {
@@ -28,4 +33,50 @@ macro_rules! impl_deserialize_for_primitive {
   };
 }
 
-impl_deserialize_for_primitive!(u8, u16, u32, u64, u128, i8, i16, i32, i64, i128);
+impl_deserialize_for_primitive!(
+    u8, u16, u32, u64, u128, i8, i16, i32, i64, i128, usize, isize, f32, f64
+);
+
+impl<'de> Deserialize<'de> for bool {
+    fn deserialize<D: Deserializer<'de>>(reader: D) -> Result<Self, D::Error> {
+        reader.deserialize_any(BoolVisitor)
+    }
+}
+
+pub struct BoolVisitor;
+
+impl BoolVisitor {
+    pub fn from_xml_str(s: &str) -> Option<bool> {
+        match s.trim().to_lowercase().as_str() {
+            "true" => Some(true),
+            "false" => Some(false),
+            "1" => Some(true),
+            "0" => Some(false),
+            "yes" => Some(true),
+            "no" => Some(false),
+            _ => None,
+        }
+    }
+}
+
+impl de::Visitor<'_> for BoolVisitor {
+    type Value = bool;
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        write!(formatter, "a string")
+    }
+
+    fn visit_text<E, V>(self, v: V) -> Result<Self::Value, E>
+    where
+        E: de::Error,
+        V: de::XmlText,
+    {
+        Self::from_xml_str(v.as_str().deref()).ok_or_else(|| E::custom("invalid value"))
+    }
+    fn visit_cdata<E, V>(self, v: V) -> Result<Self::Value, E>
+    where
+        E: de::Error,
+        V: de::XmlCData,
+    {
+        Self::from_xml_str(v.as_str().deref()).ok_or_else(|| E::custom("invalid value"))
+    }
+}
