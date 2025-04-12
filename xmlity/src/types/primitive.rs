793654ef39e7ff608e766fcd26eb5c6431eeb031
Lukas Friman <lukas@dreamplay.net>
2025-04-12 19:49:33 +0200

    feat(core): Add serialize impl for bool and (de)serialize for Box<T> (#15)
    * Added (de)serialization traits for Box<T>.
    
    * Add serialize to bool - "true" or "false".

--- xmlity/src/types/primitive.rs
+++ xmlity/src/types/primitive.rs
@@ -37,46 +37,52 @@ impl_deserialize_for_primitive!(
     u8, u16, u32, u64, u128, i8, i16, i32, i64, i128, usize, isize, f32, f64
 );
 
+impl Serialize for bool {
+    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        serializer.serialize_text(if *self { "true" } else { "false" })
+    }
+}
+
 impl<'de> Deserialize<'de> for bool {
     fn deserialize<D: Deserializer<'de>>(reader: D) -> Result<Self, D::Error> {
-        reader.deserialize_any(BoolVisitor)
-    }
-}
+        pub struct BoolVisitor;
 
-pub struct BoolVisitor;
-
-impl BoolVisitor {
-    pub fn from_xml_str(s: &str) -> Option<bool> {
-        match s.trim().to_lowercase().as_str() {
-            "true" => Some(true),
-            "false" => Some(false),
-            "1" => Some(true),
-            "0" => Some(false),
-            "yes" => Some(true),
-            "no" => Some(false),
-            _ => None,
+        impl BoolVisitor {
+            pub fn from_xml_str(s: &str) -> Option<bool> {
+                match s.trim().to_lowercase().as_str() {
+                    "true" => Some(true),
+                    "false" => Some(false),
+                    "1" => Some(true),
+                    "0" => Some(false),
+                    "yes" => Some(true),
+                    "no" => Some(false),
+                    _ => None,
+                }
+            }
         }
-    }
-}
 
-impl de::Visitor<'_> for BoolVisitor {
-    type Value = bool;
-    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-        write!(formatter, "a string")
-    }
+        impl de::Visitor<'_> for BoolVisitor {
+            type Value = bool;
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                write!(formatter, "a string")
+            }
 
-    fn visit_text<E, V>(self, v: V) -> Result<Self::Value, E>
-    where
-        E: de::Error,
-        V: de::XmlText,
-    {
-        Self::from_xml_str(v.as_str().deref()).ok_or_else(|| E::custom("invalid value"))
-    }
-    fn visit_cdata<E, V>(self, v: V) -> Result<Self::Value, E>
-    where
-        E: de::Error,
-        V: de::XmlCData,
-    {
-        Self::from_xml_str(v.as_str().deref()).ok_or_else(|| E::custom("invalid value"))
+            fn visit_text<E, V>(self, v: V) -> Result<Self::Value, E>
+            where
+                E: de::Error,
+                V: de::XmlText,
+            {
+                Self::from_xml_str(v.as_str().deref()).ok_or_else(|| E::custom("invalid value"))
+            }
+            fn visit_cdata<E, V>(self, v: V) -> Result<Self::Value, E>
+            where
+                E: de::Error,
+                V: de::XmlCData,
+            {
+                Self::from_xml_str(v.as_str().deref()).ok_or_else(|| E::custom("invalid value"))
+            }
+        }
+
+        reader.deserialize_any(BoolVisitor)
     }
 }
