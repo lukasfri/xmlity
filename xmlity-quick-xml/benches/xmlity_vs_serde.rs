d29d25c64a040ceb244d5507f6594edbbe38a81e
Lukas Friman <lukas@dreamplay.net>
2025-05-07 14:33:29 +0200

    test(quick-xml): Added `yaserde` to benchmarks.

--- xmlity-quick-xml/benches/xmlity_vs_serde.rs
+++ xmlity-quick-xml/benches/xmlity_vs_serde.rs
@@ -115,4 +115,61 @@ mod serde_bench {
     criterion_group!(benches, criterion_benchmark);
 }
 
-criterion_main!(xmlity_bench::benches, serde_bench::benches);
+mod yaserde_bench {
+    use criterion::{black_box, criterion_group, Criterion};
+    use yaserde::{YaDeserialize, YaSerialize};
+
+    #[derive(Clone, YaSerialize, YaDeserialize)]
+    #[yaserde(rename = "c")]
+    pub struct C {
+        #[yaserde(rename = "b")]
+        pub b: String,
+    }
+
+    #[derive(YaSerialize, YaDeserialize)]
+    #[yaserde(rename = "d")]
+    pub struct D {
+        pub c: Vec<C>,
+    }
+
+    pub fn deserialize<C: YaDeserialize>(xml: &str) -> C {
+        yaserde::de::from_str(xml).unwrap()
+    }
+
+    pub fn serialize<T: YaSerialize>(value: &T) -> String {
+        yaserde::ser::to_string(value).unwrap()
+    }
+
+    fn criterion_benchmark(c: &mut Criterion) {
+        c.bench_function("yaserde basic deserialize", |b| {
+            b.iter(|| deserialize::<C>(black_box(r#"<c><b>A</b></c>"#)))
+        })
+        .bench_function("yaserde basic serialize", |b| {
+            b.iter(|| serialize(&C { b: "A".to_string() }))
+        })
+        .bench_function("yaserde list deserialize", |b| {
+            let constructed_xml = format!(
+                r#"<d>{}</d>"#,
+                (0..50)
+                    .map(|_| r#"<c><b>A</b></c>"#)
+                    .collect::<Vec<_>>()
+                    .join("")
+            );
+            b.iter(|| deserialize::<D>(black_box(constructed_xml.as_str())))
+        })
+        .bench_function("yaserde list serialize", |b| {
+            b.iter(|| {
+                serialize(&D {
+                    c: vec![C { b: "A".to_string() }; 50],
+                })
+            })
+        });
+    }
+    criterion_group!(benches, criterion_benchmark);
+}
+
+criterion_main!(
+    xmlity_bench::benches,
+    serde_bench::benches,
+    yaserde_bench::benches
+);
