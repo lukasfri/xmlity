e7c2ffcd1daac3c4bb4428117d5b10e43a67e75f
Lukas Friman <lukas@dreamplay.net>
2025-06-04 11:14:18 +0200

    Added test.

--- xmlity-quick-xml/tests/elements/group_element_equivalent.rs
+++ xmlity-quick-xml/tests/elements/group_element_equivalent.rs
@@ -1,4 +1,6 @@
-use xmlity::{DeserializationGroup, Deserialize, SerializationGroup, Serialize, XmlNamespace};
+use xmlity::{
+    DeserializationGroup, Deserialize, LocalName, SerializationGroup, Serialize, XmlNamespace,
+};
 
 #[test]
 fn complex_type_alike_wrapped_element_equals_element() {
@@ -142,4 +144,132 @@ fn simple_wrapped_element_equals_element() {
     assert_eq!(a_expected, a);
 
     assert_eq!(a_expected, a_wrapped.0);
+}
+
+#[test]
+fn top_level_xs_wrapped_element_equals_element() {
+    #[derive(Debug, PartialEq, Serialize, Deserialize)]
+    #[xelement(
+        name = "attributeGroup",
+        namespace = "http://www.w3.org/2001/XMLSchema"
+    )]
+    struct AttributeGroupRefType {
+        #[xattribute(name = "ref", optional)]
+        pub ref_: Option<String>,
+        #[xvalue(default)]
+        pub annotation: Option<Anno>,
+    }
+
+    #[derive(Debug, PartialEq, Serialize, Deserialize)]
+    #[xelement(name = "element", namespace = "http://www.w3.org/2001/XMLSchema")]
+    struct LocalElement {
+        #[xattribute(name = "ref", optional)]
+        pub ref_: Option<String>,
+        #[xattribute(name = "maxOccurs", optional)]
+        pub max_occurs: Option<String>,
+        #[xvalue(default)]
+        pub annotation: Option<Anno>,
+        // #[xvalue(default)]
+        // pub type_choice: Option<types::top_level_element_items::Child1>,
+        #[xvalue(default)]
+        pub alternatives: Vec<Alt>,
+    }
+
+    #[derive(Debug, PartialEq, Serialize, Deserialize)]
+    enum GroupTypeContent {
+        Element(LocalElement),
+    }
+
+    #[derive(Debug, PartialEq, Serialize, Deserialize)]
+    #[xelement(name = "sequence", namespace = "http://www.w3.org/2001/XMLSchema")]
+    struct SequenceType {
+        pub content: Vec<LocalElement>,
+    }
+
+    #[derive(Debug, PartialEq, Serialize, Deserialize)]
+    enum TypeDefParticle {
+        Sequence(SequenceType),
+    }
+
+    #[derive(Debug, PartialEq, Serialize, Deserialize)]
+    enum ComplexTypeModel {
+        Other {
+            #[xvalue(default)]
+            open_content: Option<OpenContent>,
+            #[xvalue(default)]
+            type_def_particle: Option<TypeDefParticle>,
+            #[xvalue(default)]
+            attributes: Vec<AttributeGroupRefType>,
+        },
+    }
+
+    #[derive(Debug, PartialEq, SerializationGroup, DeserializationGroup)]
+    struct LocalComplexType {
+        #[xvalue(default)]
+        pub annotation: Option<Anno>,
+        pub content: ComplexTypeModel,
+    }
+
+    #[derive(Debug, PartialEq, Serialize, Deserialize)]
+    enum Child1 {
+        #[xelement(name = "complexType", namespace = "http://www.w3.org/2001/XMLSchema")]
+        ComplexType(#[xgroup] LocalComplexType),
+    }
+
+    #[derive(Debug, PartialEq, Serialize, Deserialize)]
+    #[xelement(name = "annotation", namespace = "http://www.w3.org/2001/XMLSchema")]
+    struct Anno;
+
+    #[derive(Debug, PartialEq, Serialize, Deserialize)]
+    #[xelement(name = "openContent", namespace = "http://www.w3.org/2001/XMLSchema")]
+    struct OpenContent;
+
+    #[derive(Debug, PartialEq, Serialize, Deserialize)]
+    #[xelement(name = "alternative", namespace = "http://www.w3.org/2001/XMLSchema")]
+    struct Alt;
+
+    #[derive(
+        Debug, PartialEq, Serialize, Deserialize, SerializationGroup, DeserializationGroup,
+    )]
+    #[xelement(
+        name = "element",
+        namespace = "http://www.w3.org/2001/XMLSchema",
+        children_order = "loose"
+    )]
+    #[xgroup(children_order = "strict")]
+    struct TopLevelElement {
+        #[xattribute(name = "name")]
+        pub name: LocalName<'static>,
+        // #[xvalue(default)]
+        // pub annotation: Option<Anno>,
+        #[xvalue(default)]
+        pub child_1: Option<Child1>,
+        #[xvalue(default)]
+        pub alternative: Option<Alt>,
+    }
+
+    #[derive(Debug, PartialEq, Serialize, Deserialize)]
+    #[xelement(name = "element", namespace = "http://www.w3.org/2001/XMLSchema")]
+    struct TopLevelElementWrapped(#[xgroup] pub TopLevelElement);
+
+    const XHTML_THEAD: &str = r###"
+    <xs:element xmlns="http://www.w3.org/1999/xhtml" xmlns:xs="http://www.w3.org/2001/XMLSchema" name="thead">
+    <xs:complexType>
+        <xs:sequence>
+        <xs:element maxOccurs="unbounded" ref="tr"/>
+        </xs:sequence>
+        <xs:attributeGroup ref="attrs"/>
+        <xs:attributeGroup ref="cellhalign"/>
+        <xs:attributeGroup ref="cellvalign"/>
+    </xs:complexType>
+    </xs:element>
+    "###;
+
+    let a: TopLevelElement = xmlity_quick_xml::from_str(XHTML_THEAD.trim()).unwrap();
+    println!("{:?}", a);
+
+    let a_wrapped: TopLevelElementWrapped = xmlity_quick_xml::from_str(XHTML_THEAD.trim()).unwrap();
+    println!("{:?}", a_wrapped);
+
+    assert_eq!(a, a_wrapped.0);
 }
